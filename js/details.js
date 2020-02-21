c587865e8d3489cc66535eb3e141aafd236baec2
Ole Simen Nilsen <olesimen.nilsen@gmail.com>
2020-02-21 23:13:01 +0100

    First part

---
+++ js/details.js
@@ -0,0 +1,13 @@
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+
+let id;
+
+if (params.has("id")) {
+    id = params.get("id");
+} else {
+    document.location.href = "/";
+}
+
+const baseUrl = "https://rickandmortyapi.com/api/character/";
+const characterUrl = `${baseUrl}${id}
