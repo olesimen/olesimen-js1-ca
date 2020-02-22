07fb178aad4a6484b3bbf3b9a4b32489fe5fed83
Ole Simen Nilsen <olesimen.nilsen@gmail.com>
2020-02-23 00:30:53 +0100

    Initial try

--- js/details.js
+++ js/details.js
@@ -10,4 +10,37 @@ if (params.has("id")) {
 }
 
 const baseUrl = "https://rickandmortyapi.com/api/character/";
-const characterUrl = `${baseUrl}${id}
+const characterUrl = `${baseUrl}${id}`;
+
+fetch(characterUrl)
+    .then(response => response.json())
+    .then(json => insertDetails(json))
+    .catch(error => (window.location.href = "error.html"));
+
+function insertDetails(json) {
+    const details = json;
+    console.dir(details);
+
+    // Insert character info
+    const characterImage = document.querySelector(".details-image");
+    characterImage.src = details.image;
+    characterImage.alt = details.name;
+
+    const characterName = document.querySelector("h1");
+    characterName.innerText = details.name;
+
+    const characterStatus = document.querySelector("#status");
+    characterStatus.innerText = details.status;
+
+    const characterSpecies = document.querySelector("#species");
+    characterSpecies.innerText = details.species;
+
+    const characterOrigin = document.querySelector("#origin");
+    characterOrigin.innerText = details.origin.name;
+
+    const characterLocation = document.querySelector("#location");
+    characterLocation.innerText = details.location.name;
+
+    // Page title
+    document.title = details.name;
+}
